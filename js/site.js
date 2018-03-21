bdbdb3a27528ffeb5062101d6a12c418dc4555eb
huynhan147 <huyitptit2015@gmail.com>
2018-03-21 12:03:21 +0700

    update2

--- js/site.js
+++ js/site.js
@@ -1,5 +1,8 @@
   function myFunction() {
     let show = document.getElementById('txtinput');
+    let clearsss = document.getElementsByClassName('clear');
+    // clearsss.style.display='none';
+    // console.log(clearsss);
     show.style.display="inline-block";
     let li =document.createElement("li");
     hr = document.createElement("hr");
@@ -53,6 +56,8 @@
                          }           
                               }
                          let ax =document.getElementById('item').innerHTML= dem +" Item";
+                              // let checkli = document.getElementsByTagName('LI');
+
 
             }
         }
@@ -62,7 +67,7 @@
                 for(let i =0;i<abc.length;i++){
                         abc[i].onclick =function(){
                             parentss=this.parentElement;
-                    parentss.classList.toggle('checked');
+                    		parentss.classList.toggle('checked');
                             let showli = document.getElementsByTagName('LI');
                              let dem=0;
                             for(let i =0 ;i<showli.length;i++){
@@ -72,11 +77,28 @@
                          }           
                               }
                          let ax =document.getElementById('item').innerHTML= dem +" Item";
+                let tmp=0;
+            	for(let i =0 ;i<checkli.length;i++){
+                	let classli = checkli[i].getAttribute('class');
+               			if(classli=='checked'){
+              		    	tmp=1;
+                		}
+                		if(tmp==1){
+                		clearrs = document.getElementsByClassName('clear');
+                		clearrs[0].style.display ='inline-block';
+               			 }else{
+                			clearrs[0].style.display ='none';
+                		}
+            	}
                 }
                 }
+        
 }
 
 function completed(){
+			document.getElementsByClassName('completed')[0].classList.add('activess');
+             document.getElementsByClassName('alls')[0].classList.remove('activess');
+             document.getElementsByClassName('active')[0].classList.remove('activess');
             let checkli = document.getElementsByTagName('LI');
             for(let i =0 ;i<checkli.length;i++){
                 let classli = checkli[i].getAttribute('class');
@@ -100,8 +122,21 @@ function clearss(){
                         clearss();
                 }
             }
+            for(let i =0 ;i<checkli.length;i++){
+                    checkli[i].style.display='block';
+            }
+            clearrs = document.getElementsByClassName('clear');
+                		clearrs[0].style.display ='none';
+             document.getElementsByClassName('alls')[0].classList.add('activess');
+             document.getElementsByClassName('active')[0].classList.remove('activess');
+             document.getElementsByClassName('completed')[0].classList.remove('activess');
+             // document.getElementsByClassName('alls')[0].classList.add('activess');
+
 }
 function actives(){
+			document.getElementsByClassName('active')[0].classList.add('activess');
+             document.getElementsByClassName('alls')[0].classList.remove('activess');
+             document.getElementsByClassName('completed')[0].classList.remove('activess');
             let checkli = document.getElementsByTagName('LI');
             for(let i =0 ;i<checkli.length;i++){
                 let classli = checkli[i].getAttribute('class');
@@ -115,6 +150,9 @@ function actives(){
 
 }
     function allx(){
+    		document.getElementsByClassName('alls')[0].classList.add('activess');
+             document.getElementsByClassName('completed')[0].classList.remove('activess');
+             document.getElementsByClassName('active')[0].classList.remove('activess');
         let checkli = document.getElementsByTagName('LI');
             for(let i =0 ;i<checkli.length;i++){
                     checkli[i].style.display='block';
